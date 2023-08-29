e30e7b7d6ad2b12755b7eaf6b67ae51e36cfc631
kelsS <ratherkelsey@gmail.com>
2023-08-28 20:39:05 -0400

    added ui js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@ function appFeats () {
 
     // add confetti whenever you hit 1500 cals
     console.log("app.js loaded")
+
+    //ui
+    const brkfstLabel = document.querySelector('#brkfast-label');
+    const lunchLabel = document.querySelector('#lunch-label');
+    const dinnerLabel = document.querySelector('#dinner-label');
+    const snacksLabel = document.querySelector('#snacks-label');
+    const plusButton = document.querySelectorAll('.add-btn');
+    const total = document.querySelector('#total');
+
+    console.log(brkfstLabel);
+    console.log(lunchLabel);
+    console.log(dinnerLabel);
+    console.log(snacksLabel);
+    console.log(plusButton);
+    console.log(total);
     
 }
 
