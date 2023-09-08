96de4992e3a9fd6668f36cdbabc4e9523ae9a5b1
kelsS <ratherkelsey@gmail.com>
2023-09-07 21:45:04 -0400

    finished mvp of app

--- js/app.js
+++ js/app.js
@@ -3,34 +3,84 @@
 function appFeats () {
 
     // add confetti whenever you hit 1500 cals
-    console.log("app.js loaded")
+    // console.log("app.js loaded")
 
     //ui
-    const brkfstLabel = document.querySelector('#brkfast-label');
-    const lunchLabel = document.querySelector('#lunch-label');
-    const dinnerLabel = document.querySelector('#dinner-label');
-    const snacksLabel = document.querySelector('#snacks-label');
-    const plusButton = document.querySelectorAll('.add-btn');
-    const total = document.querySelector('#total');
+    const form = document.querySelector('[data-js="form"]');
+    const brkfstField = document.querySelector('[data-js="brkfast-field"]');
+    const lunchField = document.querySelector('[data-js="lunch-field"]');
+    const dinnerField = document.querySelector('[data-js="dinner-field"]');
+    const snacksField = document.querySelector('[data-js="snacks-field"]');
+    const getTotal = document.querySelector('[data-js="get-total"]');
+    const total = document.querySelector('[data-js="calories-reached"]');
+    const totalCalsAlert = document.querySelector('[data-js="total-cals-alert"]');
+    const celebrateAlert = document.querySelector('[data-js="celebrate-alert"]');
 
-    console.log("breakfast input:")
-    console.log(brkfstLabel);
-    console.log("lunch input:")
-    console.log(lunchLabel);
-    console.log("dinner input:")
-    console.log(dinnerLabel);
-    console.log("snack input:")
-    console.log(snacksLabel);
-    console.log("add buttons:")
-    console.log(plusButton);
-    console.log("total:")
-    console.log(total);
+    // console.log("form:");
+    // console.log(form);
+    // console.log("breakfast input:");
+    // console.log(brkfstField);
+    // console.log("lunch input:");
+    // console.log(lunchField);
+    // console.log("dinner input:");
+    // console.log(dinnerField);
+    // console.log("snack input:");
+    // console.log(snacksField);
+    // console.log("get total btn:");
+    // console.log(getTotal);
+    // console.log("total:");
+    // console.log(total);
 
-    // add event listener for form inputs
-    // add event listener for add buttons
-    // add event listener for equals button which adds the total from the inputs to the total
-    
+    // prevent page reload for form
+    form.addEventListener('click', (event) => {
+        event.preventDefault();
+    });
 
+    // get user calories per meal time
+    getValues();
+
+    // get total calories for the day
+    getTotal.addEventListener('click', () => {
+        getTotalCals();
+    });
+
+    // inner functions
+    function getValues() {
+
+        brkfstField.addEventListener('focusout', () => {
+            const brkValue = brkfstField.value;
+            brkfstField.setAttribute('val', brkValue);
+        });
+
+        lunchField.addEventListener('focusout', () => {
+            const lunchValue = lunchField.value
+            lunchField.setAttribute('val', lunchValue);
+        });
+
+        dinnerField.addEventListener('focusout', () => {
+            const dinValue = dinnerField.value
+            dinnerField.setAttribute('val', dinValue);
+        });
+
+        snacksField.addEventListener('focusout', () => {
+            const snksValue = snacksField.value
+            snacksField.setAttribute('val', snksValue);
+        });
+    }
+
+    function getTotalCals() {
+        const totalCals = Number(brkfstField.value) + Number(lunchField.value) + Number(dinnerField.value) + Number(snacksField.value);
+        total.textContent = totalCals;
+
+        // calorie total message
+        totalCalsAlert.setAttribute('role', 'alert');
+
+        // celebrate alert
+        if (totalCals >= 1500) {
+            celebrateAlert.removeAttribute('hidden');
+            // console.log("min cals reached");
+        }
+    }
     
 }
 
